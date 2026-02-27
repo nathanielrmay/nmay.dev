b5a84d0f9b10fa402654e1a2872684fa1fcfab0e
Nathan May <nmay@nmay.dev>
2026-02-27 15:52:58 -0600

    UI: Update food section vertical menu

--- src/pages/wrv/food/lib/partials/pt_vertical_menu.php
+++ src/pages/wrv/food/lib/partials/pt_vertical_menu.php
@@ -1,11 +1,12 @@
 <div class="pt-vertical-menu">
 <ul style="list-style-type: square; padding-left: 20px;">
     <li> <a href="/wrv/pg_index.php">← Reviews</a> </li>
+    <hr> <br>
 
-    <hr>
-
-    <li> <a href="/wrv/food/pg_index.php">Food</a> </li>
-    <li> <a href="/wrv/food/pg_create_review.php">Create Review</a> </li>
+    <li> <a href="/wrv/food/pg_index.php">Food Reviews Home</a> </li>
     <li> <a href="/wrv/food/idc_war/pg_idc_wars.php">IDC Wars</a> </li>
+
+    <hr> <br>
+    <li> <a href="/wrv/food/pg_create_review.php">Create Review</a> </li>
 </ul>
 </div>
